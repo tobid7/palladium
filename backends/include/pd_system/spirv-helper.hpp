80b9e1d545350c466c7be374bd81b0897f1d8280
tobid7 <tobid7@outlook.de>
2026-03-18 21:21:00 +0100

    Add pdsg (palladium spirv-generator) - remove glslang from pd-system - use spirv blobs instead of glsl 460 - Use Legacy shaders for OpenGL2 backend - add devnotes

--- backends/include/pd_system/spirv-helper.hpp
+++ backends/include/pd_system/spirv-helper.hpp
@@ -1,10 +1,8 @@
 #pragma once
 
-#include <glslang/Public/ResourceLimits.h>
-#include <glslang/Public/ShaderLang.h>
-
 #include <pd/common.hpp>
 
+struct TBuiltInResource;
 namespace PD {
 class SpirvHelper {
  public:
