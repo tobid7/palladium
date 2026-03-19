fc76a4f3fef14533e9373b38ad648690e14ce81c
tobid7 <tobid7@outlook.de>
2026-03-19 22:06:58 +0100

    Add Drawlist - Add Pool iterator support - Add Pool Expandability - Add Pool::Push - Add Lithium Maths API - Remove InitPools - update spirv-helper

---
+++ include/pd/lithium/math.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <pd/core/core.hpp>
+#include <pd/lithium/rect.hpp>
+
+namespace PD {
+namespace Li {
+namespace Math {
+PD_API bool InBounds(const fvec2& pos, const fvec2& size, const fvec4& rect);
+PD_API bool InBounds(const fvec2& pos, const fvec4& rect);
+PD_API bool InBounds(const fvec2& a, const fvec2& b, const fvec2& c,
+                     const fvec4& rect);
+PD_API void RotateCorner(fvec2& pos, float sinus, float cosinus);
+PD_API Rect PrimRect(const fvec2& pos, const fvec2& size, float angle = 0.f);
+PD_API Rect PrimLine(const fvec2& a, const fvec2& b, int t = 1);
+}  // namespace Math
+}  // namespace Li
+}  // namespace PD
--- source/lithium/pools.cpp
+++ source/lithium/pools.cpp
@@ -6,11 +6,6 @@ namespace Li {
 PD::Pool<Vertex> pVtxPool;
 PD::Pool<u16> pIdxPool;
 
-PD_API void InitPools(size_t max_vertices) {
-  pVtxPool.Init(max_vertices);
-  pIdxPool.Init(max_vertices * 2);
-}
-
 PD_API Vertex* AllocateVertices(size_t count) {
   return pVtxPool.Allocate(count);
 }
