8de1484f0a94a47ad50c53cf18c6a451f8adbced
TobiD7 <tobid7@outlook.de>
2026-03-26 21:01:43 +0100

    implement font deletion

--- tests/gfx/include/os-ctx.hpp
+++ tests/gfx/include/os-ctx.hpp
@@ -29,6 +29,8 @@ class OsCtx {
     return fvec2(in.x * pViewPort.y, in.y * pViewPort.y);
   }
 
+  const PD::ivec2& GetViewport() const { return pViewPort; }
+
  protected:
   PD::ivec2 pViewPort;
   const Driver pDriver;
