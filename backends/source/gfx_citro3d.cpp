f7c2e7fa4fefc28562636cdbc614424fc49a09ab
tobid7 <tobid7@outlook.de>
2026-04-03 12:52:30 +0200

    rename PDBackendFlags to PDGfxBackendFlags

--- backends/source/gfx_citro3d.cpp
+++ backends/source/gfx_citro3d.cpp
@@ -119,7 +119,7 @@ void GfxCitro3D::SysInit() {
   if (impl) return;
   PDLOG("GfxCitro3D::SysInit();");
   impl = new Impl();
-  Flags |= PDBackendFlags_FlipUV_Y;
+  Flags |= PDGfxBackendFlags_FlipUV_Y;
   impl->pShaderRaw = Pica::AssembleCode(LIShaderCTR);
   impl->pCode = DVLB_ParseFile((uint32_t*)impl->pShaderRaw.data(),
                                impl->pShaderRaw.size());
