fd19c48f739c17f125efe5e48914b7e79bce4674
tobid7 <tobid7@outlook.de>
2026-04-03 14:22:51 +0200

    Allow implicit usage of "#ffffffff" etc

--- include/pd/core/color.hpp
+++ include/pd/core/color.hpp
@@ -72,6 +72,12 @@ class PD_API Color {
   constexpr Color(const std::string_view& hex) { Hex(hex); }
 
   /**
+   * Constructor for Hex Input (is abel to run at compile time xD)
+   * @param hex Hex String in `#ffffff` or `#ffffffff` format
+   */
+  constexpr Color(const char* hex) { Hex(std::string_view{hex}); }
+
+  /**
    * Create Color Object by Hex String (at compile time btw)
    * @param hex Hex String in `#ffffff` or `#ffffffff` format
    * @return Color class itself
