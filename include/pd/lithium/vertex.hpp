25607b865cca01d92c6252240d6d5e47a8c97728
tobid7 <tobid7@outlook.de>
2026-09-01 21:21:40 +0200

    faster rect command creation

--- include/pd/lithium/vertex.hpp
+++ include/pd/lithium/vertex.hpp
@@ -9,6 +9,8 @@ class Vertex {
   Vertex() {}
   Vertex(const fvec2& pos, const fvec2& uv, u32 color)
       : pos(pos), uv(uv), color(color) {}
+  Vertex(float x, float y, float u, float v, u32 color)
+      : pos(x, y), uv(u, v), color(color) {}
   ~Vertex() {}
 
   void Reset() {
