d54c94c8ea5427c2ed5af4b75de9942c446e6feb
TobiD7 <tobid7@outlook.de>
2026-09-06 16:18:06 +0200

    Revert "Rework Project Ultra - Added container - Made container the base of Layout - Set Containers theirself as pParent for Elements on Push"
    This reverts commit 996ab4486748d476150c65d1094578bd026eb4dd.

--- include/pd/ultra/elems/element.hpp
+++ include/pd/ultra/elems/element.hpp
@@ -6,7 +6,6 @@
 namespace PD {
 namespace Ultra {
 class Canvas;
-class Container;
 class PD_API ElementBase {
  public:
   ElementBase() {}
@@ -17,6 +16,7 @@ class PD_API ElementBase {
    * Reset Function (for PD::Pool::FastReset)
    */
   virtual void Reset() {}
+  void Update(Canvas& c);
 
   void SetAlignment(UltraAlignment a) { pAlignment = a; }
   void SetPosition(const PD::fvec2& pos) { pPos = pos; }
@@ -25,14 +25,13 @@ class PD_API ElementBase {
   void SetSize(float w, float h) { pSize = PD::fvec2(w, h); }
 
  protected:
-  friend class Container;
-  void SetParent(Container* c) { pParent = c; }
   bool RevisionUpdate(PD::u32 req);
-  Container* pParent;
   PD::u32 pCanvasRev = 0;
+  PD::Li::Rect pRenderspace;
   UltraAlignment pAlignment = 0;
   PD::fvec2 pPos;
   PD::fvec2 pSize;
+  ElementBase* pParent = nullptr;
 };
 }  // namespace Ultra
 }  // namespace PD
--- source/ultra/layout.cpp
+++ source/ultra/layout.cpp
@@ -2,11 +2,14 @@
 
 namespace PD {
 namespace Ultra {
+PD_API void Layout::Add(ElementBase& elem) { *pElements.Allocate() = &elem; }
+
 PD_API void Layout::SetFont(PD::Li::Font& font) { pList.SetFont(&font); }
 
 PD_API void Layout::Render() {
   pList.Clear();
-  for (auto& it : GetElements()) {
+  for (auto& it : pElements) {
+    it->Update(pCanvas);
     it->Draw(pList);
   }
 }
