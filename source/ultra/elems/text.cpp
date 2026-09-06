21ca6bbf586a158acb57d9eada7cce40dd6224b5
TobiD7 <tobid7@outlook.de>
2026-09-06 16:18:06 +0200

    Revert "WIP (Codename Ultra) UI lib"
    This reverts commit 19f19e807262dd43c18b9e8e2512cfc36c62d6c0.

--- source/ultra/elems/text.cpp
+++
@@ -1,9 +0,0 @@
-#include <pd/ultra/elems/text.hpp>
-
-namespace PD {
-namespace Ultra {
-PD_API void Text::Draw(PD::Li::Drawlist& l) {
-  l.DrawText(pPos, pText.c_str(), pColor);
-}
-}  // namespace Ultra
-}  // namespace PD
